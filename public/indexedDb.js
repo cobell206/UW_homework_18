0e445e5070ccb0c67441a76935184fa23dcb2f83
Colin Bell <cobell206@gmail.com>
2021-05-11 11:10:36 -1000

    function working, service worker almost there

---
+++ public/indexedDb.js
@@ -0,0 +1,75 @@
+let db
+let budgetVersion
+const dbName = 'budgetStorage'
+
+var request = indexedDB.open('BudgetDB', budgetVersion || 21)
+
+function checkForIndexedDb() {
+    if (!window.indexedDB) {
+      console.log("Your browser doesn't support a stable version of IndexedDB.");
+      return false;
+    }
+    return true;
+  }
+
+request.onupgradeneeded = function (event) {
+    const { oldVersion } = event
+    const newVersion = event.newVersion || db.newVersion
+
+    db = event.target.result
+    if (db.objectStoreNames.length === 0) {
+        db.createObjectStore(dbName, {autoIncrement: true})
+    }
+}
+  
+function emptyDatabase() {
+       
+    let tx = db.transaction([dbName], 'readwrite')
+
+    const store = tx.objectStore(dbName)
+    const getAll = store.getAll()
+
+    getAll.onsuccess = function () {
+
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                  }
+            })
+                .then(response => response.json)
+                .then(res => {
+                    if (res.length !== 0) {
+                        transaction = db.transaction([dbName], 'readwrite')
+                        const currentStore = transaction.objectStore(dbName)
+                        currentStore.clear()
+                        console.log(`Cleared store from ${dbName}`);
+                    }
+                })
+        }
+    }
+}
+
+request.onsuccess = function (event) {
+    db = event.target.result
+    if (navigator.onLine) {
+        console.log('Back online');
+        emptyDatabase()
+    }
+}
+
+  
+const saveRecord = (record) => {
+
+    const transaction = db.transaction(dbName, 'readwrite')
+
+    const store = transaction.objectStore(dbName)
+    store.add(record)
+
+    console.log('Record saved');
+}
+
+window.addEventListener('online', emptyDatabase)
